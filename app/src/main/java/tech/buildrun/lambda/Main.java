1f9cc08809e34039bcc692d9071a19cdd46f97c7
Victor Dias <95438984+diasvictordev@users.noreply.github.com>
2024-11-05 15:14:04 -0300

    Update Main.java

--- app/src/main/java/tech/buildrun/lambda/Main.java
+++ app/src/main/java/tech/buildrun/lambda/Main.java
@@ -1,10 +1,7 @@
 package tech.buildrun.lambda;
 
 
-public class Main {
-
-    public static void main(String[] args) {
-       Handler handler = new Handler();
-        handler.sendRequest();
+ public static void main(String[] args) {
+        Handler handler = new Handler();
+        handler.helloWorld();
     }
-}
