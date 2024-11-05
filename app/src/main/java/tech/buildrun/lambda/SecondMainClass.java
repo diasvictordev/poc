2a1d4427bfd0d8603ca9ee8216f5e4d2fa2ef006
Victor Dias <95438984+diasvictordev@users.noreply.github.com>
2024-11-05 14:00:03 -0300

    Create SecondMainClass.java

---
+++ app/src/main/java/tech/buildrun/lambda/SecondMainClass.java
@@ -0,0 +1,9 @@
+package tech.buildrun.lambda;
+
+public class SecondMain {
+
+    public static void main(String[] args) {
+        // Simulação de um request para teste
+        System.out.println("Hello world! 2");
+    }
+}
