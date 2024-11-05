4913670f1ab043309cb0bac262aef18f6396355b
Victor Dias <95438984+diasvictordev@users.noreply.github.com>
2024-11-05 13:49:44 -0300

    Create Main.java

---
+++ app/src/main/java/tech/buildrun/lambda/Main.java
@@ -0,0 +1,9 @@
+package tech.buildrun.lambda;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Simulação de um request para teste
+        System.out.println("Hello world!");
+    }
+}
