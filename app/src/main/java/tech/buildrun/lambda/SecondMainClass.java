4f0647145a695c88a4616cf1bb453eda62545bb4
Victor Dias <95438984+diasvictordev@users.noreply.github.com>
2024-11-05 14:02:17 -0300

    Update SecondMainClass.java

--- app/src/main/java/tech/buildrun/lambda/SecondMainClass.java
+++ app/src/main/java/tech/buildrun/lambda/SecondMainClass.java
@@ -1,6 +1,6 @@
 package tech.buildrun.lambda;
 
-public class SecondMain {
+public class SecondMainClass {
 
     public static void main(String[] args) {
         // Simulação de um request para teste
